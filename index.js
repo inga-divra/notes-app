4fb0aa8daddbcdc9e6b7179cae5497c3d2dc417d
inga-divra <ingady@gmail.com>
2025-04-14 17:24:56 +0300

    feat: add dist static files to backend for frontend deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path'); // Include the 'path' module to handle file paths correctly
 
 let notes = [
   {
@@ -20,6 +21,7 @@ let notes = [
   },
 ];
 
+// Request logger middleware
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:  ', request.path);
@@ -32,10 +34,10 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
+// API routes
 app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
@@ -51,12 +53,14 @@ app.get('/api/notes/:id', (request, response) => {
   }
 });
 
+// Function to generate a new ID
 const generateId = () => {
   const maxId =
     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
   return String(maxId + 1);
 };
 
+// Route for creating a new note
 app.post('/api/notes', (request, response) => {
   const body = request.body;
 
@@ -77,6 +81,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note);
 });
 
+// Route for updating a note
 app.put('/api/notes/:id', (request, response) => {
   const id = request.params.id;
   const body = request.body;
@@ -96,6 +101,7 @@ app.put('/api/notes/:id', (request, response) => {
   response.json(note);
 });
 
+// Route for deleting a note
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id;
   notes = notes.filter((note) => note.id !== id);
@@ -103,12 +109,14 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
+// Middleware for handling unknown endpoints
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
 
+// Start the server on specified port
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
