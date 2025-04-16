f8c15137f411f5a728608b88acdbcecdcd5b1bdd
inga-divra <ingady@gmail.com>
2025-04-16 14:26:06 +0300

    Handle errors when fetching a single note by ID
    - Return 404 if note not found in the database
    - Return 400 and error message if ID is malformatted
    - Log error details to console for easier debugging

--- index.js
+++ index.js
@@ -48,9 +48,18 @@ app.post('/api/notes', (request, response) => {
 
 // Get SINGLE NOTE
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: 'malformatted id' });
+    });
 });
 
 // Function to generate a new ID
