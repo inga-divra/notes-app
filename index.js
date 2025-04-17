f68ac8ef794f306d05ac5754715075d66c41271a
inga-divra <ingady@gmail.com>
2025-04-17 08:17:21 +0300

    feat(api): add schema validation and centralized error handling
    - Added Mongoose validators to note schema (minlength, required for content)
    - Updated POST /api/notes to use .catch and next(error)
    - Extended errorHandler middleware to handle ValidationError

--- index.js
+++ index.js
@@ -29,21 +29,21 @@ app.get('/api/notes', (request, response) => {
 });
 
 // Create NEW NOTE
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'Content missing' });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+
+    .catch((error) => next(error));
 });
 
 // Get SINGLE NOTE
@@ -100,9 +100,13 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
+
   next(error);
 };
+
 app.use(errorHandler);
 
 // Start the server on specified port
