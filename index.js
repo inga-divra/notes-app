6f82eb6ffbbe8101552f35ea3cd300ce2dfd55b4
inga-divra <ingady@gmail.com>
2025-04-17 19:07:56 +0300

    Fix: Remove eslint-plugin-prettier, update ESLint config and Prettier settings for consistent code style

--- index.js
+++ index.js
@@ -1,116 +1,115 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const path = require('path'); // Include the 'path' module to handle file paths correctly
-const Note = require('./models/note');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path') // Include the 'path' module to handle file paths correctly
+const Note = require('./models/note')
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 //Logger middleware
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
-app.use(requestLogger);
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
 
 // Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // Get ALL NOTES
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
 // Create NEW NOTE
 app.post('/api/notes', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-  });
+    important: body.important || false
+  })
 
   note
     .save()
     .then((savedNote) => {
-      response.json(savedNote);
+      response.json(savedNote)
     })
 
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Get SINGLE NOTE
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // UPDATE Note
 app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body;
+  const { content, important } = request.body
 
   Note.findById(request.params.id)
     .then((note) => {
       if (!note) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      note.content = content;
-      note.important = important;
+      note.content = content
+      note.important = important
 
       return note.save().then((updatedNote) => {
-        response.json(updatedNote);
-      });
+        response.json(updatedNote)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // DELETE Note
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Middleware for handling unknown endpoints
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-app.use(unknownEndpoint);
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 // Middleware for handling errors
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 // Start the server on specified port
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const Note = require('./models/note');
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const url = `mongodb+srv://ingady:${password}@nodeexpressprojects.shkmp.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NodeExpressProjects`;
+const password = process.argv[2]
+const url = `mongodb+srv://ingady:${password}@nodeexpressprojects.shkmp.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NodeExpressProjects`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 // Create a new note
 const note = new Note({
   content: 'HTML is easy',
-  important: true,
-});
+  important: true
+})
 
 // Save the note to the database
-note.save().then((result) => {
-  console.log('note saved!');
+note.save().then(() => {
+  console.log('note saved!')
 
   // Fetch all notes from the database
   Note.find({}).then((result) => {
     result.forEach((note) => {
-      console.log(note);
-    });
-    mongoose.connection.close();
-  });
-});
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
