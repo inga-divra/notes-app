b5c03ebce5709256058e40568dd5dd70255f2bf1
inga-divra <ingady@gmail.com>
2025-05-02 17:22:07 +0300

    test: add user creation test and usersInDb helper
    - Added test for user creation to ensure new users can be added successfully
    - Integrated usersInDb helper to check database state before and after user creation

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,12 @@
 const assert = require('node:assert')
 const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -108,6 +110,39 @@ describe('when there is initially some notes saved', () => {
   })
 })
 
+//USER TESTS
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+})
 after(async () => {
   await mongoose.connection.close()
 })
