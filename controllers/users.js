1ff3a0199daf95ce97e987277f0980f5a3a782c3
inga-divra <ingady@gmail.com>
2025-05-02 18:16:46 +0300

    updated controllers

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,16 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+//GET ALL USERS
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('notes', {
+    content: 1,
+    important: 1
+  })
+
+  response.json(users)
+})
+
 //CREATE NEW USER
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
@@ -18,12 +28,6 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
-})
-
-//GET ALL USERS
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
 })
 
 module.exports = usersRouter
