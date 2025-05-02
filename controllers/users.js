deaf656ae6f1950e8befdd9b9c6c491bc7893574
inga-divra <ingady@gmail.com>
2025-05-02 17:52:14 +0300

    added user tests

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+//CREATE NEW USER
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
@@ -17,6 +18,12 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+//GET ALL USERS
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
 })
 
 module.exports = usersRouter
