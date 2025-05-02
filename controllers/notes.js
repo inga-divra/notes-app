759f9035ef4b009a6077a0f1d8a665e5fda07d57
inga-divra <ingady@gmail.com>
2025-05-02 15:50:22 +0300

    refactor: use async/await in GET and DELETE routes for note operations

--- controllers/notes.js
+++ controllers/notes.js
@@ -25,16 +25,17 @@ notesRouter.post('/', async (request, response, next) => {
 })
 
 // Get SINGLE NOTE
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 // UPDATE Note
@@ -58,12 +59,13 @@ notesRouter.put('/:id', (request, response, next) => {
 })
 
 // DELETE Note
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Note.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = notesRouter
