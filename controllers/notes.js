77d7aac903c35d4a0f9e64808fd059d0a9740c35
inga-divra <ingady@gmail.com>
2025-05-02 17:58:10 +0300

    Add user association to notes and update routes for note creation

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 // GET all notes
 notesRouter.get('/', async (request, response) => {
@@ -26,12 +27,18 @@ notesRouter.put('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false
+    important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
